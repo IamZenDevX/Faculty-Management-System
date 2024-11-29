5cd896137586543968bb50038c1437ca6ce1e8a0
IamZenDevX <zendevx.social@gmail.com>
2024-11-29 14:31:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-
 const PORT = process.env.PORT || 1312;
-
 const studentRouter = require("./Routes/studentRoutes");
 const courseRouter = require("./Routes/courseRoutes");
 
@@ -16,6 +14,14 @@ app.use("/", logging);
 app.use("/api/students", studentRouter);
 app.use("/api/courses", courseRouter);
 
-app.listen(PORT, () => {
-  console.log("http://localhost: " + PORT);
-});
+
+const dbURI = `mongodb://localhost:27017/`;
+
+mongoose
+.connect(dbURI)
+.then(() => {
+  app.listen(3000);
+  console.log("MongoDB connected...");
+})
+.catch((err) => console.log(err));
+app.use(ErrorHandler)
