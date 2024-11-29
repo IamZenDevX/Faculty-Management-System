131af8cbd36778a616b20740910d15bba6427c30
IamZenDevX <zendevx.social@gmail.com>
2024-11-29 14:32:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 1312;
 const studentRouter = require("./Routes/studentRoutes");
